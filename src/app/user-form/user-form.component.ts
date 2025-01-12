66b2c3af40b16bf541482057f838d7d05e071952
disha-thakur-neudesic <disha.thakur@neudesic.com>
2025-01-12 17:01:54 +0530

    --adding formsModule and formbuilder --adding user interface

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.css'],
 })
-export class UserFormComponent {}
+export class UserFormComponent implements OnInit {
+  userForm: FormGroup = new FormGroup({});
+
+  constructor(private formbuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.userForm = this.formbuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      inputEmail: ['', [Validators.required, Validators.email]],
+      phone: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {}
+}
